b57233ba9e2e33816137379b5b28d693f4360f21
Soroush MasoomBabaei <soroushcars@gmail.com>
2024-12-12 00:46:25 +0330

    Going to have conflict!

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import random
 
 
 def main():
+    print("Hello, World@OfficialParsaNaderi")
     print("Hello, World@SoroushMBabaei")
     ran_num = random.randrange(10, 100, 10)
     for i in range(ran_num):
