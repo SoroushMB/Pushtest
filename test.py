1252f8e840b6aa84fbc03d2901f46f5237e95ba8
Soroush MasoomBabaei <soroushcars@gmail.com>
2024-12-12 00:43:04 +0330

    Going to have conflict!

--- test.py
+++ test.py
@@ -2,11 +2,10 @@ import random
 
 
 def main():
-    print("Hello, World")
+    print("Hello, World@SoroushMBabaei")
     ran_num = random.randrange(10, 100, 10)
     for i in range(ran_num):
         print(i)
 
 if __name__ == "__main__":
     main()
-
