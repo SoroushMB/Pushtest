28e4c1e853c3a06523ea4f3359ce4f51b590f1ff
Soroush MasoomBabaei <soroushcars@gmail.com>
2024-12-12 00:07:47 +0330

    Second commit! Still alive!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pushtest
+
 This repo is a test for pushing programs to the Github!
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello, World")
+def main():
+    print("Hello, World")
+    for i in range(10):
+        print(i)
+
+if __name__ == "__main__":
+    main()
+
